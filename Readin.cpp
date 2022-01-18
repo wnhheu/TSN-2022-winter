d8211a41f7069174394483de6a715449a05b16d0
wnhheu <77094755+wnhheu@users.noreply.github.com>
2022-01-18 14:04:04 +0800

    面向对象_改进

---
+++ Readin.cpp
@@ -0,0 +1,23 @@
+#include "Readin.h"
+#include <bits/stdc++.h>
+#include <iostream>
+void ReadIn::read()
+{
+    std::string line;
+    std::string filename;
+    std::ifstream in("D:\\Users\\Documents\\GitHub\\TSN-2022-winter\\Data\\DelayRange\\dc2\\topo.csv");
+    if (in.fail())
+    {
+        std::cout << "File not found" << std::endl;
+        return;
+    }
+    int cnt = 0;
+    while(getline(in, line) && in.good())
+    {
+        ++cnt;
+        if(cnt == 1)
+            continue;
+        split(links, line);  //把line里的单元格数字字符提取出来，“,”为单元格分隔符
+    }
+    in.close();
+}
